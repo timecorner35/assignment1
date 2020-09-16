a9dfdcd1aed438093233f37662ac641ea80e6b32
timecorner35 <71405627+timecorner35@users.noreply.github.com>
2020-09-16 19:59:45 +0100

    Add files via upload

---
+++ day_2/Question8.java
@@ -0,0 +1,22 @@
+package day_2;
+
+public class Question8 {
+	public static void main(String[] args) {
+		
+		System.out.println("*       *" + "\t*       *" + "\t*       *");
+		System.out.println(" *     * " + "\t*       *" + "\t *     * ");
+		System.out.println("  *   *  " + "\t*       *" + "\t  *   *  ");
+		System.out.println("   * *   " + "\t*       *" + "\t   * * 	");
+		System.out.println("    *    " + "\t*********" + "\t    *");
+		System.out.println("    *    " + "\t*       *" + "\t    *");
+		System.out.println("    *    " + "\t*       *" + "\t    *");
+		System.out.println("    *    " + "\t*       *" + "\t    *");
+		System.out.println("    *    " + "\t*       *" + "\t    *");
+		
+	
+		
+		
+		
+		
+}
+}
